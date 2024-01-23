3e59e043b20a1081b8576d2320cbd75c40d4d0a2
LiuJingZ <346197701@qq.com>
2024-01-23 15:17:46 +0800

    fix: page

--- packages/client/src/components/herder/index.tsx
+++ packages/client/src/components/herder/index.tsx
@@ -1,233 +1,141 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import style from "./index.module.css";
 // import { useDrawPanel } from '@/providers/DrawPanelProvider.tsx'
-import { clsx } from 'clsx'
-import { useRenderGrid } from '../../hooks/useRenderGrid'
-import DrawPanel from '../shared/DrawPanel'
-import { CANVAS_HEIGHT, CANVAS_WIDTH, MAX_ROWS_COLS } from '../../global/constants'
+import { clsx } from "clsx";
+import { useRenderGrid } from "../../hooks/useRenderGrid";
+import DrawPanel from "../shared/DrawPanel";
+import {
+  CANVAS_HEIGHT,
+  CANVAS_WIDTH,
+  MAX_ROWS_COLS,
+} from "../../global/constants";
+
 
 export default function Header() {
-    // const {
-    //     cellSize,
-    //     coordinates,
-    //     selectedHexColor,
-    //     data,
-    //     panOffsetX,
-    //     panOffsetY,
-    //     setPanOffsetX,
-    //     setPanOffsetY,
-    //     onCellClick,
-    //     onVisibleAreaCoordinate,
-    //     onHover,
-    //   } = useDrawPanel()
-
-      
   const [numberData, setNumberData] = useState(50);
   const gridCanvasRef = React.useRef(null);
   const [panning, setPanning] = useState(false);
-  const [panOffsetX, setPanOffsetX] = useState(0);
-  const [panOffsetY, setPanOffsetY] = useState(0);
+
   const [GRID_SIZE, setGRID_SIZE] = useState(64);
-
-//   const [ initialPositionX, setInitialPositionX ] = React.useState<number>(0)
-//   const [ initialPositionY, setInitialPositionY ] = React.useState<number>(0)
-//   const visibleAreaXStart = Math.max(0, Math.floor(-panOffsetX / cellSize))
-//   const visibleAreaYStart = Math.max(0, Math.floor(-panOffsetY / cellSize))
-
-//   // max: [x,y]: [20,20]
-//   const visibleAreaXEnd = Math.min(MAX_ROWS_COLS, Math.ceil((CANVAS_WIDTH - panOffsetX) / cellSize))
-//   const visibleAreaYEnd = Math.min(MAX_ROWS_COLS, Math.ceil((CANVAS_HEIGHT - panOffsetY) / cellSize))
-
-  // Add a new state for storing the mousedown time
-  const [ mouseDownTime, setMouseDownTime ] = React.useState<number>(0)
   const btnLower = () => {
     setNumberData(numberData - 5); // 每次点击减号减少5
-    setGRID_SIZE(GRID_SIZE-5)
+    setGRID_SIZE(GRID_SIZE - 5);
   };
   const btnAdd = () => {
     setNumberData(numberData + 5); // 每次点击加号增加5
-    setGRID_SIZE(GRID_SIZE+5)
+    setGRID_SIZE(GRID_SIZE + 5);
   };
-//   const gridCanvasRef = React.useRef<HTMLCanvasElement>() as any
-  const renderGrid = useRenderGrid()
-//   React.useEffect(() => {
-//     if (gridCanvasRef.current) {
-//       const ctx = gridCanvasRef.current.getContext('2d', { willReadFrequently: true })
-//       if (!ctx) return
+  const renderGrid = useRenderGrid();
 
-//       renderGrid(ctx, {
-//         width: gridCanvasRef.current.width,
-//         height: gridCanvasRef.current.height,
-//         cellSize,
-//         coordinates,
-//         panOffsetX,
-//         panOffsetY,
-//         selectedHexColor,
-//         visibleAreaXStart,
-//         visibleAreaXEnd,
-//         visibleAreaYStart,
-//         visibleAreaYEnd,
-//         pixels: data,
-//         focus
-//       })
-//     }
-//   }, [ coordinates, panOffsetX, panOffsetY, cellSize, selectedHexColor, data, renderGrid, visibleAreaXStart, visibleAreaXEnd, visibleAreaYStart, visibleAreaYEnd ])
-// function onClickCoordinates(clientX: number, clientY: number) {
-//     if (!gridCanvasRef.current) return
+  const CANVAS_WIDTH = window.innerWidth;
+  const CANVAS_HEIGHT = window.innerHeight;
 
-//     const rect = gridCanvasRef.current.getBoundingClientRect()
-//     const x = Math.abs(panOffsetX) + clientX - rect.left  // pixel
-//     const y = Math.abs(panOffsetY) + clientY - rect.top  // pixel
+  const [offsetX, setOffsetX] = useState(0);
+  const [offsetY, setOffsetY] = useState(0);
+  const [hoveredSquare, setHoveredSquare] = useState<{
+    x: number;
+    y: number;
+  } | null>(null);
+  const startXRef = useRef<number>(0);
+  const startYRef = useRef<number>(0);
+  const offsetXRef = useRef<number>(offsetX);
+  const offsetYRef = useRef<number>(offsetY);
+  const [selectedColor, setSelectedColor] = useState("#2f1643");
+  // // 为每个颜色选项添加点击事件监听器
+  // const colorOptions = document.querySelectorAll('.color-option');
+  // colorOptions.forEach((option) => {
+  //   option.addEventListener('click', handleColorOptionClick);
+  // });
+  // 点击事件处理程序
+  function handleColorOptionClick(color: any) {
+    setSelectedColor(color);
+  }
 
-//     const gridX = Math.floor(x / cellSize)
-//     const gridY = Math.floor(y / cellSize)
+  useEffect(() => {
+    const canvas = gridCanvasRef.current as any;
+    const ctx = canvas.getContext("2d");
 
-//     onCellClick?.([ gridX, gridY ])
-//   }
+    // 设置画布大小
+    canvas.width = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
 
-//   function onMouseLeave() {
-//     setPanning(false)
-
-//     onVisibleAreaCoordinate?.([ visibleAreaXStart, visibleAreaYStart ], [ visibleAreaXEnd, visibleAreaYEnd ])
-//   }
-
-//   function onMouseUp(event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) {
-//     setPanning(false)
-//     onVisibleAreaCoordinate?.([ visibleAreaXStart, visibleAreaYStart ], [ visibleAreaXEnd, visibleAreaYEnd ])
-
-//     // If the time difference between mouse down and up is less than a threshold (e.g., 200ms), it's a click
-//     if (Date.now() - mouseDownTime < 300) {
-//       onClickCoordinates(event.clientX, event.clientY)
-//     }
-//   }
-
-//   function onMouseDown(clientX: number, clientY: number) {
-//     setPanning(true)
-//     setInitialPositionX(clientX - panOffsetX)
-//     setInitialPositionY(clientY - panOffsetY)
-
-//     // Record the current time when mouse is down
-//     setMouseDownTime(Date.now())
-//   }
-
-//   function onMouseMove(clientX: number, clientY: number) {
-//     if (panning) {
-//     // this is a negative value
-//     const offsetX = clientX - initialPositionX;
-//     const offsetY = clientY - initialPositionY;
-
-//     const maxOffsetX = -(MAX_ROWS_COLS * cellSize - CANVAS_WIDTH) ; // Maximum allowed offset in X direction
-//     const maxOffsetY = -(MAX_ROWS_COLS * cellSize - CANVAS_WIDTH) ; // Maximum allowed offset in Y direction
-
-//     setPanOffsetX(offsetX > 0 ? 0 : Math.abs(offsetX) > Math.abs(maxOffsetX) ? maxOffsetX : offsetX)
-//     setPanOffsetY(offsetY > 0 ? 0 : Math.abs(offsetY) > Math.abs(maxOffsetY) ? maxOffsetY : offsetY)
-//     } else {
-//       onMouseHover(clientX, clientY)
-//     }
-//   }
-//   function onMouseHover(clientX: number, clientY: number) {
-//     if (!gridCanvasRef.current) return
-
-//     const rect = gridCanvasRef.current.getBoundingClientRect()
-//     const x = Math.abs(panOffsetX) + clientX - rect.left  // pixel
-//     const y = Math.abs(panOffsetY) + clientY - rect.top  // pixel
-
-//     const gridX = Math.floor(x / cellSize)
-//     const gridY = Math.floor(y / cellSize)
-
-//     // Now you have the grid coordinates on hover, you can use them as you need
-//     onHover?.([ gridX, gridY ])
-//   }
-const CANVAS_WIDTH = 1850;
-const CANVAS_HEIGHT = 830;
-// const GRID_SIZE = 64;
-
-React.useEffect(() => {
-  const canvas = gridCanvasRef.current as any;
-  const ctx = canvas.getContext('2d');
-
-  // 绘制网格函数
-  const renderGrid = (gridSize: number) => {
     // 清空画布
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // 移动画布坐标系
 
-    // 绘制格子
-    for (let x = -panOffsetX; x < canvas.width - panOffsetX; x += gridSize) {
-      for (let y = -panOffsetY; y < canvas.height - panOffsetY; y += gridSize) {
-        ctx.fillStyle = 'gray';
-        ctx.fillRect(x, y, gridSize, gridSize);
+    ctx.translate(offsetX, offsetY);
+    // 绘制小方格
+    for (let x = GRID_SIZE; x <= CANVAS_WIDTH; x += GRID_SIZE) {
+      for (let y = GRID_SIZE; y <= CANVAS_HEIGHT; y += GRID_SIZE) {
+        ctx.strokeStyle = "#000000";
+        ctx.fillStyle = "#2f1643";
+        ctx.strokeRect(x - GRID_SIZE, y - GRID_SIZE, GRID_SIZE, GRID_SIZE);
+        ctx.fillRect(x - GRID_SIZE, y - GRID_SIZE, GRID_SIZE, GRID_SIZE);
       }
     }
 
-    // 绘制文本
-    ctx.font = `15px Arial`;
-    ctx.fillStyle = 'black';
-    ctx.textBaseline = 'middle';
-    ctx.textAlign = 'center';
-    for (let x = -panOffsetX; x < canvas.width - panOffsetX; x += gridSize) {
-      for (let y = -panOffsetY; y < canvas.height - panOffsetY; y += gridSize) {
-        const text = `(${x / gridSize},${y / gridSize})`;
-        ctx.fillText(text, x + gridSize / 2, y + gridSize / 2);
-      }
+    // 绘制鼠标悬停的方格背景色为蓝色
+    if (hoveredSquare) {
+      const { x, y } = hoveredSquare;
+      ctx.fillStyle = 'blue';
+      ctx.fillRect(x, y, GRID_SIZE, GRID_SIZE);
     }
-  };
 
-  // 绑定鼠标事件
-  const handleMouseMove = (event:any) => {
-    if (panning) {
-      const rect = canvas.getBoundingClientRect();
-      const offsetX = event.clientX - rect.left;
-      const offsetY = event.clientY - rect.top;
-      setPanOffsetX(panOffsetX + (offsetX - panStartX));
-      setPanOffsetY(panOffsetY + (offsetY - panStartY));
-      panStartX = offsetX;
-      panStartY = offsetY;
-    }
-  };
-
-  let panStartX = 0;
-  let panStartY = 0;
-  const handleMouseDown = (event:any) => {
+    // 检查鼠标位置是否在画布范围内
+  }, [
+    hoveredSquare,
+    offsetX,
+    offsetY,
+    selectedColor,
+    CANVAS_HEIGHT,
+    CANVAS_WIDTH,
+    GRID_SIZE,
+  ]);
+  const handleMouseDown = (event: any) => {
     setPanning(true);
-    panStartX = event.clientX - canvas.getBoundingClientRect().left;
-    panStartY = event.clientY - canvas.getBoundingClientRect().top;
-
-    const offsetX = panStartX + panOffsetX;
-    const offsetY = panStartY + panOffsetY;
-
-    const gridX = Math.floor(offsetX / GRID_SIZE);
-    const gridY = Math.floor(offsetY / GRID_SIZE);
-
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(gridX * GRID_SIZE, gridY * GRID_SIZE, GRID_SIZE, GRID_SIZE);
+    startXRef.current = event.clientX;
+    startYRef.current = event.clientY;
   };
+  const handleMouseMove = (event: any) => {
+    const canvas = gridCanvasRef.current as any;
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
 
-  canvas.addEventListener('mousedown', handleMouseDown);
+    if (panning) {
+      const offsetXChange = event.clientX - startXRef.current;
+      const offsetYChange = event.clientY - startYRef.current;
+      setOffsetX((prevOffsetX) => prevOffsetX + offsetXChange);
+      setOffsetY((prevOffsetY) => prevOffsetY + offsetYChange);
+      startXRef.current = event.clientX;
+      startYRef.current = event.clientY;
+    }
+
+    if (
+      mouseX >= 0 &&
+      mouseY >= 0 &&
+      mouseX <= canvas.width &&
+      mouseY <= canvas.height
+    ) {
+      const squareX = Math.floor((mouseX - offsetX) / GRID_SIZE) * GRID_SIZE;
+      const squareY = Math.floor((mouseY - offsetY) / GRID_SIZE) * GRID_SIZE;
+
+      setHoveredSquare({ x: squareX, y: squareY });
+    } else {
+      setHoveredSquare(null);
+    }
+  };
 
   const handleMouseUp = () => {
+    offsetXRef.current = offsetX;
+    offsetYRef.current = offsetY;
+    startXRef.current = 0;
+    startYRef.current = 0;
     setPanning(false);
   };
-
-  const handleMouseLeave = () => {
-    setPanning(false);
+  const handleLeave = () => {
+    setHoveredSquare(null);
   };
-
-  canvas.addEventListener('mousemove', handleMouseMove);
-  canvas.addEventListener('mousedown', handleMouseDown);
-  canvas.addEventListener('mouseup', handleMouseUp);
-  canvas.addEventListener('mouseleave', handleMouseLeave);
-
-  renderGrid(GRID_SIZE);
-
-  return () => {
-    canvas.removeEventListener('mousemove', handleMouseMove);
-    canvas.removeEventListener('mousedown', handleMouseDown);
-    canvas.removeEventListener('mouseup', handleMouseUp);
-    canvas.removeEventListener('mouseleave', handleMouseLeave);
-  };
-}, [GRID_SIZE, numberData, panOffsetX, panOffsetY]);
-
-
   return (
     <>
       <div className={style.container}>
@@ -254,19 +162,22 @@ React.useEffect(() => {
         </div>
         <div className={style.addr}>address</div>
       </div>
+
       {/* <DrawPanel/> */}
       <div className={style.bodyCon}>
-        <canvas ref={gridCanvasRef}
-        
-                  width={CANVAS_WIDTH}
-                  height={CANVAS_HEIGHT}
-                  className={clsx([ 'cursor-pointer', { '!cursor-grab': panning } ])}
-                //   onMouseDown={(event) => onMouseDown(event.clientX, event.clientY)}
-                //   onMouseMove={(event) => onMouseMove(event.clientX, event.clientY)}
-                //   onMouseUp={(event) => onMouseUp(event)}
-                //   onMouseLeave={onMouseLeave}
-          />
-          </div>
+        <canvas
+          ref={gridCanvasRef}
+          id="gridCanvas"
+          width={CANVAS_WIDTH}
+          height={CANVAS_HEIGHT}
+          className={clsx(["cursor-pointer", { "!cursor-grab": panning }])}
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          onMouseMove={handleMouseMove}
+          onMouseLeave={handleLeave}
+        />
+       
+      </div>
     </>
   );
 }
