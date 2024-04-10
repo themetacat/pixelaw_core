8f7d23e1622f7c3ef64ae3d528b0a6880bdc0e76
MJumpKing <2572430586@qq.com>
2024-04-10 13:20:42 +0800

    update init game

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo -e "Run 'pnpm install' to install the dependencies."
-cd packages/contracts
+# echo -e "Run 'pnpm install' to install the dependencies."
 #pnpm install
 
 echo -e "Check if anvil is running('pnpm mud deploy' need this)."
@@ -12,8 +11,30 @@ then
     exit 0
 fi
 
-echo -e "Run 'pnpm mud deploy' to deploy contracts to RPC http://127.0.0.1:8545, which anvil listening on."
-pnpm mud deploy
+RPC_URL="http://127.0.0.1:8545"
+CHAIN_ID="31337"
+
+for arg in "$@"; do
+    # 使用等号分割键值对
+    key=$(echo "$arg" | cut -d '=' -f1)
+    value=$(echo "$arg" | cut -d '=' -f2-)
+    
+    # 根据键进行处理
+    case $key in
+        RPC_URL)
+            RPC_URL=$value
+            ;;
+        CHAIN_ID)
+            CHAIN_ID=$value
+            ;;
+    esac
+done
+
+echo -e "Run 'pnpm mud deploy' to deploy contracts to RPC , which anvil listening on."
+cd packages/contracts
+pnpm mud deploy --rpc $RPC_URL
+
+WORLD_ADDRESS=$(cat ./worlds.json | jq -r --arg chain_id $CHAIN_ID '.[$chain_id].address')
 
 echo -e "Run 'pnpm vite' to start the frontend server of PixeLAW Core, which will listening on http://127.0.0.1:3000."
 cd ../client
@@ -26,14 +47,22 @@ fi
 
 echo -e "Register call_system to World contract."
 cd ../call_system
-forge script script/CallSystemExtension.s.sol --rpc-url http://127.0.0.1:8545 --broadcast
+forge script script/CallSystemExtension.s.sol --rpc-url $RPC_URL --broadcast
+sleep 1
 
 echo -e "Register default app paint to World contract."
 cd ../paint
-forge script script/PaintExtension.s.sol --rpc-url http://127.0.0.1:8545 --broadcast
+forge script script/PaintExtension.s.sol --rpc-url $RPC_URL --broadcast
+PRIVATE_KEY=$(grep -E "^PRIVATE_KEY=" .env | cut -d '=' -f2-)
+PRIVATE_KEY=${PRIVATE_KEY:-'0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6'}
+cast send $WORLD_ADDRESS --rpc-url $RPC_URL --private-key $PRIVATE_KEY "paint_PaintSystem_init()" ""
+sleep 1
 
 echo -e "Register default app snake to World contract."
 cd ../snake
-forge script script/SnakeExtension.s.sol --rpc-url http://127.0.0.1:8545 --broadcast
+forge script script/SnakeExtension.s.sol --rpc-url $RPC_URL --broadcast
+PRIVATE_KEY=$(grep -E "^PRIVATE_KEY=" .env | cut -d '=' -f2-)
+PRIVATE_KEY=${PRIVATE_KEY:-'0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6'}
+cast send $WORLD_ADDRESS --rpc-url $RPC_URL --private-key $PRIVATE_KEY "snake_SnakeSystem_init()" ""
 
 echo -e "Congratulations! Everything is ok! Just visit http://127.0.0.1:3000 to play."
