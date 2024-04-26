576e27846e6bbfa1eb5ec625d44bb45345d231f1
MJumpKing <2572430586@qq.com>
2024-04-26 18:41:15 +0800

    open right

--- packages/client/src/components/rightPart/index.tsx
+++ packages/client/src/components/rightPart/index.tsx
@@ -61,7 +61,7 @@ export default function RightPart({
     systemCalls: { update_abi },
   } = useMUD();
   const entities_app = useEntityQuery([Has(App)]);
-  const [panning, setPanning] = useState(false);
+  const [panning, setPanning] = useState(true);
   const loacl_app_name = window.localStorage.getItem("app_name");
 
   // const coorToEntityID = (x: number, y: number) => encodeEntity({ x: "uint32", y: "uint32" }, { x, y });
