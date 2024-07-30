814a4f2e643f61d352c341ba6e507d407f7943cf
The30th <66099098+MJumpKing@users.noreply.github.com>
2024-07-30 09:57:43 +0800

    close faucet

--- packages/client/src/mud/setupNetwork.ts
+++ packages/client/src/mud/setupNetwork.ts
@@ -227,7 +227,7 @@ export async function setupNetwork(): Promise<SetupNetworkResult> {
                 }
                
             }
-            if(networkConfig.chain.id === 31337 || networkConfig.chain.id === 31338){
+            if(networkConfig.chain.id === 31337){
               
               requestDrip();
               setInterval(requestDrip, 20000)
